3ac2b8da09e0a9246643e6a55faabcc8ce8f864c
Denis <volt@gmail.com>
2022-01-17 16:08:49 +0300

    add cycle and condition in programm

---
+++ test.js
@@ -0,0 +1,16 @@
+let a = 'abc';
+
+
+console.log(a.length >= 3);
+
+
+// switch (a,b) {
+//     case "","":
+//         alert("Пожалуйста, пройдите опрос!");
+//         i = 0;
+//     case a.length > 50:
+//         alert("Название фильма больше 50 символов!");
+//         i = 0;
+//     default:
+//         personalMovieDB.movies[a] = b;
+// }
